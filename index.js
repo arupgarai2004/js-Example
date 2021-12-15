06fc37607aa1df0cd590a18d04fec2d8b89630f8
arupgarai2004 <arupgarai2004@gmail.com>
2021-12-15 20:17:38 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 const arr =[4,3,5,7]; 
-
 const output=arr.map((x)=>x*2); // const output=arr.map((x)=>x*2); 
 console.log(output);
 
