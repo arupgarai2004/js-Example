8b896a80f704459924e7303a6a170ba32ddab3e1
arupgarai2004 <arupgarai2004@gmail.com>
2022-01-23 21:01:32 +0530

    1 file modified

--- callBindApply.js
+++ callBindApply.js
@@ -5,9 +5,15 @@ let name ={
 		console.log( this.firstname +" "+ this.lastName +" "+state +" "+city);
 	}
 }
-name.printFullName("Karnataka","Bangalore");
+
 let name2={
 	firstname:"Sachin",
 	lastName:"Tendulkar",
 }
-name.printFullName.call(name2,"MH","Mumbai");// print fullname borrowed from name object .
+
+name.printFullName.call(name2,'WB','Kolkata'); //Call argment as parameter 
+name.printFullName.apply(name2,['DL','Delhi']); // apply argument as array 
+let bindData=name.printFullName.bind(name2,'MH','Mumbai'); // functtion need to invoke separatly 
+bindData();
+
+
