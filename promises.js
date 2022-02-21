5ca4b1143e51361c2c6ed7f6f4661a7d89530c19
arupgarai2004 <arupgarai2004@gmail.com>
2022-02-21 23:14:16 +0530

    1 file modified

--- promises.js
+++ promises.js
@@ -1,14 +1,30 @@
-const promise = new Promise(
-  (resolve) => {
+////first example
+const promise = new Promise((resolve, reject) => {
+  var a = 3;
+  if (a == 2) {
     setTimeout(() => {
       resolve('Promise has been excuted sucessfully!');
     }, 5000);
-  },
-  (reject) => {}
-);
-
-promise.then((value) => {
-  console.log(value);
+  } else {
+    reject('Promise has been rejected');
+  }
 });
+
+promise
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((message) => {
+    console.log(message);
+  });
+
+/// call API using fetch , which is a in build promise
+let data = fetch('https://gorest.co.in/public/v2/users');
+
+data.then((value) => {
+  return value.json();
+}).then((dataAray)=>{
+  console.log(dataAray);
+})
 
 console.log('promise page End');
