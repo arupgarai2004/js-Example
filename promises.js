e8e0241e0c123de9c5d3cdc874ee2389e54e9bb1
arupgarai2004 <arupgarai2004@gmail.com>
2022-02-21 22:47:52 +0530

    1 file modified

--- promises.js
+++ promises.js
@@ -1,9 +1,11 @@
 const promise = new Promise(resolve => {
   setTimeout(() => {
-    resolve("I'm a Promise!");
+    resolve("Promise has been excuted sucessfully!");
   }, 5000);
 }, reject => {
 
 });
 
-promise.then(value => console.log(value));
+promise.then((value) => {
+  console.log(value); 
+});
